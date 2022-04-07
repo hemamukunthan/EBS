4fc68e1f53b43b4a682b4999a013eb07f534e9c1
Hema M <hemamukunthan02@gmail.com>
2022-04-07 21:54:53 +0530

    Second Commit

--- src/L1_10_EBS/TeamMembers.java
+++ src/L1_10_EBS/TeamMembers.java
@@ -7,8 +7,6 @@ public class TeamMembers extends javax.swing.JFrame {
         initComponents();
         setLocationRelativeTo(null);
     }
-
-    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -144,9 +142,7 @@ public class TeamMembers extends javax.swing.JFrame {
         new Welcome().setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
